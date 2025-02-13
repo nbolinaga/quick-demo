cef78bfe1255690393c694eeb5256ab7c276f104
nbolinaga <nbolinaga17@gmail.com>
2025-02-13 11:48:30 -0500

    ADDED: mobiles fixes

--- packages/nextjs/app/position/page.tsx
+++ packages/nextjs/app/position/page.tsx
@@ -6,8 +6,8 @@ import Chart from "~~/components/Chart";
 const page = () => {
   return (
     <div className="relative flex items-center flex-col flex-grow pt-10 h-screen bg-cover bg-fixed bg-center bg-[url('/stars.jpeg')] overflow-hidden ">
-      <p className="text-white text-3xl">Historical PAXG closing price</p>
-      <div className="mx-auto mt-20 h-[40vh] space-y-5 w-2/3">
+      <p className="text-white text-2xl md:text-3xl">Historical PAXG closing price</p>
+      <div className="mx-auto mt-20 h-[40vh] space-y-5 w-full md:w-2/3">
         <Chart />
       </div>
     </div>
