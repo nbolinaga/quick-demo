0c6237522c6e58bc5e5dfbe8942665880792120a
nbolinaga <nbolinaga17@gmail.com>
2025-02-13 11:24:27 -0500

    ADDED: data file for product selection

---
+++ packages/nextjs/app/data.ts
@@ -0,0 +1,33 @@
+export const RWASelectData = [
+  {
+    key: "PAXG (Gold)",
+    value: "Pax Gold",
+  },
+  {
+    key: "KAG (Silver)",
+    value: "Kinesis Silver",
+  },
+  {
+    key: "APFC (Agriland)",
+    value: "APF Coin",
+  },
+  {
+    key: "REG (Real estate)",
+    value: "RealToken",
+  },
+  {
+    key: "AGRI (Agricultural)",
+    value: "Agridex",
+  },
+];
+
+export const StrategySelectData = [
+  {
+    key: "Trend Following (Momentum)",
+    value: "Momentum",
+  },
+  {
+    key: "Reduced Volatility (Smoothcoin)",
+    value: "Smoothcoin",
+  },
+];
